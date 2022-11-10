f3316bd7ef79495d9079657d341a8a98530d065b
Филипп <karkasson@yandex.ru>
2022-11-11 01:23:08 +0300

    functionality for getting the whole map of persons is added fix v1.0

--- src/main/java/com/example/demo/GetAllServlet.java
+++ src/main/java/com/example/demo/GetAllServlet.java
@@ -8,11 +8,19 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Map;
+import java.util.Set;
+
 @WebServlet(name="getAll", urlPatterns = "/getAll")
 public class GetAllServlet extends HttpServlet {
     public void doGet(HttpServletRequest request,
                       HttpServletResponse response) throws ServletException, IOException {
         PrintWriter out = response.getWriter();
-        out.println(PersonStore.getInstance().getAll());
+        Map<Integer, Person> allPersons = PersonStore.getInstance().getAll();
+        //
+        Set<Integer> ids = allPersons.keySet();
+        for(Integer id : ids) {
+            out.println(id + " " + allPersons.get(id).getName() + " " + allPersons.get(id).getAge());
+        }
     }
 }
